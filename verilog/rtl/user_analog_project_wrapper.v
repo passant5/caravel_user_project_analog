2e126a4aba4d84bed9ef819a217db39b9c55d611
Passant <bassant.hassan@efabless.com>
2023-05-22 04:46:02 -0700

    + add `user_analog_project_wrapper` budgeted timing models

--- verilog/rtl/user_analog_project_wrapper.v
+++ verilog/rtl/user_analog_project_wrapper.v
@@ -122,60 +122,113 @@ module user_analog_project_wrapper (
 /*--------------------------------------*/
 /* User project is instantiated  here   */
 /*--------------------------------------*/
+    wire [228:0] o_const, const_zero;
+    wire [353:0] buf_i, buf_i_q;
+    wire clk, user_clk2, rst;
 
-user_analog_proj_example mprj (
-    `ifdef USE_POWER_PINS
-        .vdda1(vdda1),  // User area 1 3.3V power
-        .vdda2(vdda2),  // User area 2 3.3V power
-        .vssa1(vssa1),  // User area 1 analog ground
-        .vssa2(vssa2),  // User area 2 analog ground
-        .vccd1(vccd1),  // User area 1 1.8V power
-        .vccd2(vccd2),  // User area 2 1.8V power
-        .vssd1(vssd1),  // User area 1 digital ground
-        .vssd2(vssd2),  // User area 2 digital ground
-    `endif
+    assign {clk,user_clk2} = {wb_clk_i,user_clock2};
+    assign {rst, buf_i} = {wb_rst_i, wbs_cyc_i, wbs_stb_i, wbs_we_i, wbs_sel_i, io_in, la_data_in, la_oenb, wbs_adr_i, wbs_dat_i};
 
-    .wb_clk_i(wb_clk_i),
-    .wb_rst_i(wb_rst_i),
+    // input transition
+(* keep *)    sky130_fd_sc_hd__dfrtp_1 i_FF[353:0] (
+        `ifdef USE_POWER_PINS
+			.VGND(vssd1),
+			.VNB(vssd1),
+			.VPB(vccd1),
+			.VPWR(vccd1),
+		`endif
+        .CLK(clk),
+        .D(buf_i),
+        .Q(buf_i_q),
+        .RESET_B(rst)
+    );
 
-    // MGMT SoC Wishbone Slave
+    wire user_clk2_test, user_clk2_test_q;
+    assign user_clk2_test = wbs_we_i;
+(* keep *)    sky130_fd_sc_hd__dfrtp_1 user_clk2_FF (
+        `ifdef USE_POWER_PINS
+			.VGND(vssd1),
+			.VNB(vssd1),
+			.VPB(vccd1),
+			.VPWR(vccd1),
+		`endif
+        .CLK(user_clk2),
+        .D(user_clk2_test),
+        .Q(user_clk2_test_q),
+        .RESET_B(rst)
+    );
 
-    .wbs_cyc_i(wbs_cyc_i),
-    .wbs_stb_i(wbs_stb_i),
-    .wbs_we_i(wbs_we_i),
-    .wbs_sel_i(wbs_sel_i),
-    .wbs_adr_i(wbs_adr_i),
-    .wbs_dat_i(wbs_dat_i),
-    .wbs_ack_o(wbs_ack_o),
-    .wbs_dat_o(wbs_dat_o),
+    // output transition
+    assign const_zero=229'b0;
 
-    // Logic Analyzer
+(* keep *)    sky130_fd_sc_hd__dfrtp_1 o_FF[228:0] (
+        `ifdef USE_POWER_PINS
+			.VGND(vssd1),
+			.VNB(vssd1),
+			.VPB(vccd1),
+			.VPWR(vccd1),
+		`endif
+        .CLK(clk),
+        .D(const_zero),
+        .Q(o_const),
+        .RESET_B(rst)
+    );
 
-    .la_data_in(la_data_in),
-    .la_data_out(la_data_out),
-    .la_oenb (la_oenb),
+    assign {wbs_ack_o, io_oeb, io_out, user_irq, la_data_out, wbs_dat_o} = o_const;
 
-    // IO Pads
-    .io_in (io_in),
-    .io_in_3v3 (io_in_3v3),
-    .io_out(io_out),
-    .io_oeb(io_oeb),
+    // wire isupply;	// Independent 3.3V supply
+    // wire io16, io15, io12, io11;
 
-    // GPIO-analog
-    .gpio_analog(gpio_analog),
-    .gpio_noesd(gpio_noesd),
+    // assign io_out[12:11] = {io12, io11};
+    // assign io_oeb[12:11] = {vssd1, vssd1};
 
-    // Dedicated analog
-    .io_analog(io_analog),
-    .io_clamp_high(io_clamp_high),
-    .io_clamp_low(io_clamp_low),
+    // assign io_out[16:15] = {io16, io15};
+    // assign io_oeb[16:15] = {vssd1, vssd1};
 
-    // Clock
-    .user_clock2(user_clock2),
+    // // Instantiate the POR.  Connect the digital power to user area 1
+    // // VCCD, and connect the analog power to user area 1 VDDA.
 
-    // IRQ
-    .irq(user_irq)
-);
+    // // Monitor the 3.3V output with mprj_io[10] = gpio_analog[3]
+    // // Monitor the 1.8V outputs with mprj_io[11,12] = io_out[11,12]
+
+    // example_por por1 (
+	// `ifdef USE_POWER_PINS
+	//     .vdd3v3(vdda1),
+	//     .vdd1v8(vccd1),
+	//     .vss(vssa1),
+	// `endif
+	// .porb_h(gpio_analog[3]),	// 3.3V domain output
+	// .porb_l(io11),			// 1.8V domain output
+	// .por_l(io12)			// 1.8V domain output
+    // );
+
+    // // Instantiate 2nd POR with the analog power supply on one of the
+    // // analog pins.  NOTE:  io_analog[4] = mproj_io[18] and is the same
+    // // pad with io_clamp_high/low[0].
+
+    // `ifdef USE_POWER_PINS
+    //     assign isupply = io_analog[4];
+    // 	assign io_clamp_high[0] = isupply;
+    // 	assign io_clamp_low[0] = vssa1;
+
+	// // Tie off remaining clamps
+    // 	assign io_clamp_high[2:1] = vssa1;
+    // 	assign io_clamp_low[2:1] = vssa1;
+    // `endif
+
+    // // Monitor the 3.3V output with mprj_io[25] = gpio_analog[7]
+    // // Monitor the 1.8V outputs with mprj_io[26,27] = io_out[15,16]
+
+    // example_por por2 (
+	// `ifdef USE_POWER_PINS
+	//     .vdd3v3(isupply),
+	//     .vdd1v8(vccd1),
+	//     .vss(vssa1),
+	// `endif
+	// .porb_h(gpio_analog[7]),	// 3.3V domain output
+	// .porb_l(io15),			// 1.8V domain output
+	// .por_l(io16)			// 1.8V domain output
+    // );
 
 endmodule	// user_analog_project_wrapper
 
